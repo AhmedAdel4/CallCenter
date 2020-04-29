71ea99de25ada3b1be20c9773f346a889106e0ee
Ahmed Adel <ahmedcrspo55@gmail.com>
2020-04-29 23:35:22 +0200

    fix PDF problem

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -2,17 +2,16 @@
 
 namespace App\Http\Controllers\User;
 
-use PDF;
 use DateTime;
 use App\Calls;
 use App\Status;
 use App\Sources;
 use Carbon\Carbon;
 use Prophecy\Call\Call;
+use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
-use Barryvdh\DomPDF\PDF as DomPDFPDF;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
@@ -86,7 +85,7 @@ class HomeController extends Controller
         {
             return response()->json(['errors' => 'لا توجد بيانات']);
         }
-        $pdf = new PDF(['tempDir' => __DIR__ . '/']);
+        
         $pdf = PDF::loadView('HomePage.pdf', ['results' => $results]);
         return $pdf->stream('data.pdf');
     }
--- config/dompdf.php
+++ config/dompdf.php
@@ -57,8 +57,9 @@ return array(
          * The temporary directory is required to download remote images and when
          * using the PFDLib back end.
          */
-        "temp_dir" => sys_get_temp_dir(),
+        // "temp_dir" => sys_get_temp_dir(),
 
+        "temp_dir" => storage_path('temp/'),
         /**
          * ==== IMPORTANT ====
          *
